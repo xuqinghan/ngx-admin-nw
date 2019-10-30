02ce7f7481b6b6c16cd27f62ac8fb38fe80cd0cd
xuqinghan <xuqinghan@gmail.com>
2019-10-30 10:08:11 +0800

    move ngx-admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,21 @@
-import { Component } from '@angular/core';
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from './@core/utils/analytics.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
 })
+export class AppComponent implements OnInit {
 
-export class AppComponent {
-  title = 'nw-angular';
-  isDev = window.nw.process.versions['nw-flavor'] === 'sdk';
-
-  versions = '' +
-    'You are running NW.js (v' + window.nw.process.versions.nw + ' ' + window.nw.process.versions['nw-flavor'] + '), ' +
-    'Node.js (v' + window.nw.process.versions.node + '), ' +
-    'Chromium (v' + window.nw.process.versions.chromium + '), ' +
-    'and Angular (v8.0.3).';
-
-  public links: any;
-
-  constructor () {
-    this.links = [
-      {
-        title: 'Angular Tutorial',
-        url: 'https://angular.io/tutorial'
-      },
-      {
-        title: 'Angular CLI Documentation',
-        url: 'https://angular.io/cli'
-      },
-      {
-        title: 'NW.js Documentation',
-        url: 'https://nwjs.io'
-      }
-    ];
+  constructor(private analytics: AnalyticsService) {
   }
 
-  public open (evt, link) {
-    evt.preventDefault();
-    window.nw.Shell.openExternal(link.url);
-  }
-
-  public openDevTools (evt) {
-    evt.preventDefault();
-    window.nw.Window.get().showDevTools();
+  ngOnInit(): void {
+    this.analytics.trackPageViews();
   }
 }
